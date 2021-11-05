dcc644e71339392b83afe267fbf6eab225e5cff6
simoonp <simoonp2@gmail.com>
2021-11-05 10:16:38 +0800

    修改conversion.sh，为输出文件添加md格式的颜色

--- English/conversion.sh
+++ English/conversion.sh
@@ -4,7 +4,7 @@ sed '/2021/d;/^\s*$/d' $1 > mcc
 file="mcc"
 echo "------"
 j=0
-save="c"$1
+save="s"$1
 for i in $(cat $file)
 do
     j=$((j+1))
@@ -16,15 +16,38 @@ do
 done
 rm mcc
 
-sed -i 's/\[31m//' $save
-sed -i 's/\[32m//' $save
-sed -i 's/\[33m//' $save
-sed -i 's/\[0m//' $save
-sed -i 's/\[36m//' $save
-sed -i 's/\[34m//' $save
-sed -i 's/\[36m//' $save
-sed -i 's/\[31m//' $save
-sed -i 's/\[0m//' $save
+# 一下格式方便终端显示
+# sed -i 's/\[31m//' $save
+# sed -i 's/\[31m//' $save
+# sed -i 's/\[32m//' $save
+# sed -i 's/\[33m//' $save
+# sed -i 's/\[0m//' $save
+# sed -i 's/\[36m//' $save
+# sed -i 's/\[34m//' $save
+# sed -i 's/\[36m//' $save
+# sed -i 's/\[31m//' $save
+# sed -i 's/\[0m//' $save
+# sed -i 's///' $save
+# sed -i 's///' $save
+# sed -i 's///' $save
+# sed -i 's///' $save
+# s
+# sed -i G $save  # 添加空行
+
+# 以下方便md格式查看
+sed -i 's/\[31m/<font color=Red>/' $save	# 将终端红色转换为md红色
+sed -i 's/\[31m/<font color=Red>/' $save	
+sed -i 's/\[31m/<font color=Red>/' $save	
+sed -i 's/\[32m/<font color=Green>/' $save	# 将终端绿色转换为md绿色	
+sed -i 's/\[33m/<font color=Yellow>/' $save	# 将终端黄色转换为md黄色
+sed -i 's/\[34m/<font color=Blue>/' $save	# 将终端蓝色转换为md蓝色
+sed -i 's/\[36m/<font color=Teal>/' $save	# 将终端蓝绿色转换为md蓝绿色
+sed -i 's/\[36m/<font color=Teal>/' $save
+sed -i 's/\[36m/<font color=Teal>/' $save
+
+sed -i 's/\[0m/<\/font>/' $save	# 匹配<font>标志
+sed -i 's/\[0m/<\/font>/' $save
+
 sed -i 's///' $save
 sed -i 's///' $save
 sed -i 's///' $save
---
+++ English/ctest.md
@@ -0,0 +1,7 @@
+<font color=Red>sun</font>
+英 <font color=Teal>[sʌn]</font>  美 <font color=Teal>[sʌn]</font>
+<font color=Blue>n. 太阳</font>
+<font color=Blue>vt. 使晒</font>
+<font color=Blue>vi. 晒太阳</font>
+<font color=Blue>n. (Sun)人名；(中)孙(普通话·威妥玛)；(柬)孙；(缅)素；(土)松</font>
+<font color=Red>CET4 TEM4</font>  
